fd36ca7d863dbf021b427e4bd80702dc395dbf55
0v0baek <qorqor4592@gmail.com>
2023-05-09 19:09:42 +0900

    :bug:Fix: migration 되지 않는 오류 수정
    articles/views.py : import하는 부분에 .빠져있어 구동되지 않았던 문제 수정

--- articles/views.py
+++ articles/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
-from models import Article
-from serializers import ArticleDetailSerializer
+from .models import Article
+from .serializers import ArticleDetailSerializer
 
 class HomeView(APIView):
     def get(self, request):
