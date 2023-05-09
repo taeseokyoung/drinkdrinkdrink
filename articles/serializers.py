363e2c8fb4b9ef26efd300d80f19bce96a9671ac
Hyuk Jun Kim <nmkk1234@naver.com>
2023-05-09 20:27:07 +0900

    ✨Feat: 댓글기능 추가(articles)#5
    23.05.09.
    articles/urls.py : url수정(comment 부분)
    articles/views.py : 댓글 crud 기능 추가
    articles/serializers.py : 댓글 조회용,댓글 수정용serializer 추가

---
+++ articles/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Article, Comment
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    # comment 불러오는 건 comment 시리얼라이저 만들어지면 수정!
+    # comments = 코멘트시리얼라이저(many=True)
+    likes = serializers.StringRelatedField(many=True)
+
+    # user를 user_id로 보여줌
+    def get_user(self, obj):
+        return obj.user.user_id
+
+    class Meta:
+        model = Article
+        fields = "__all__"
+
+
+# serializer 추가
+class CommentSerializer(serializers.ModelSerializer):
+    # UserSerializer 있다고 가정
+    user = serializers.UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        exclude = ("article", "created_at")
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("content",)
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from .models import Article
+from .serializers import CommentCreateSerializer, CommentSerializer
 
 
 class HomeView(APIView):
@@ -7,53 +11,80 @@ class HomeView(APIView):
         """
         HOME
         """
-        return Response({"message":"get!"})
-    
+        return Response({"message": "get!"})
+
 
 class ArticleWriteView(APIView):
     def post(self, request):
         """
         게시글 작성 페이지
         """
-        return Response({"message":"post!"})
-    
+        return Response({"message": "post!"})
+
 
 class ArticleDetailView(APIView):
     def get(self, request, article_id):
         """
         상세 게시글 보기 / 댓글 띄우기
         """
-        return Response({"message":"get!"})
-    
+        return Response({"message": "get!"})
+
     def put(self, request, article_id):
         """
         상세 게시글 수정
         """
-        return Response({"message":"put!"})
-    
+        return Response({"message": "put!"})
+
     def delete(self, request, article_id):
         """
         상세 게시글 삭제
         """
-        return Response({"message":"delete!"})
-    
+        return Response({"message": "delete!"})
+
 
 class LikeView(APIView):
     def post(self, request):
         """한 번 누르면 좋아요.
-            두 번 누르면 좋아요를 취소합니다."""
-        return Response({"message":"좋아요 누르기"})
+        두 번 누르면 좋아요를 취소합니다."""
+        return Response({"message": "좋아요 누르기"})
 
 
+# comment 클래스 추가
 class CommentView(APIView):
-    def post(self, request):
-        '''댓글 쓰기'''
-        return Response({"message":"댓글 작성"})
+    def get(self, request, article_id):
+        article = Article.objects.get(pk=article_id)
+        comments = article.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, reuqest, comment_id):
-        '''댓글 수정'''
-        return Response({"message":"댓글 수정"})
+    def post(self, request, article_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(
+                user=request.user, article=Article.objects.get(pk=article_id)
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, comment_id):
-        '''댓글 삭제'''
-        return Response({"message": "댓글 삭제"})
+
+class CommentDetailView(APIView):
+    def put(self, request, article_id, comment_id):
+        comment = Article.objects.get(pk=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, article_id, comment_id):
+        comment = get_object_or_404(Article, pk=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response("삭제완료", status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
